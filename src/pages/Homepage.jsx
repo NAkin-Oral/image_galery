d808b34e6d8941cbe1d4f0bd58837e18b5522224
nao <nurettinakinoral@gmail.com>
2022-10-20 21:10:10 +0300

    Images

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,15 +5,14 @@ const Homepage = () => {
   const [photos, setPhotos] = useState([]);
   useEffect(() => {
     fetch(`https://cwbarry.pythonanywhere.com/image/`)
-      .then(data => data.json())
-      .then(data => setPhotos(data));
+      .then(res => res.json())
+      .then(data => setPhotos(data.images));
   }, []);
-  const { images } = photos;
   return (
     <div>
       <h1>Image Galery</h1>
       <ul className="pictures">
-        {images?.map((item, index) => {
+        {photos?.map((item, index) => {
           return <Picture key={index} item={item} />;
         })}
       </ul>
